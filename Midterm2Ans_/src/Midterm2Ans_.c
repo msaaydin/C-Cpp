4e563294a323ca8798b23cf0a7b5b7dfecda7871
Musa <maydin@fsm.edu.tr>
2015-12-14 19:17:43 +0200

    master
    14122015midterm

--- Midterm2Ans_/src/Midterm2Ans_.c
+++ Midterm2Ans_/src/Midterm2Ans_.c
@@ -10,13 +10,88 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+//soru2:
+ struct Student{
+   char *ad_soyad;
+   int numara;
+   int notlar[5];
+};
+int biggestStudentId(struct Student s[]){
 
-/*int main(void) {
-	puts("Midterm 2 solution");
-	return EXIT_SUCCESS;
+	  int average[] = {0,0,0,0,0};
+      int i,j;
+      printf("fonksiyon calisti\n");
+     printf("/-----------------------------/\n");
+     for (j = 0; j < 5; j++){
+       for (i = 0; i < 5; i++){
+            average[j]+=s[j].notlar[i];
+        }
+       average[j] /=5;
+       printf("%d indexli ogrencinin ortalamasi...:%d\n",j,average[j]);
+     }
+     int max = average[0];
+     average[2] = 32;
+     int indx = 0;
+     for(i = 1; i < 5; i++){
+        if (average[i] > max){
+            max = average[i];
+            indx  = i;
+        }
 
-}*/
-//soru 7:
+     }
+return s[indx].numara;
+}
+//soru 2:
+int main(void) {
+	srand ( time(NULL) );
+	struct Student ss[5];
+    ss[0].ad_soyad = "musa aydýn";
+    ss[1].ad_soyad = "ali";
+    ss[2].ad_soyad = " aydýn";
+    ss[3].ad_soyad = "hasan";
+    ss[4].ad_soyad = "veli";
+    ss[0].numara = 2343;
+    ss[1].numara = 787;
+    ss[2].numara = 27873;
+    ss[3].numara = 999099;
+    ss[4].numara = 456644;
+    puts("Midterm 2 solution");
+    int i,j;
+    for (j = 0; j < 5; j++){
+       for (i = 0; i < 5; i++){
+          ss[j].notlar[i] = rand() % 100 + 1;
+       }
+    }
+    for (j = 0; j < 5; j++){
+           for (i = 0; i < 5; i++){
+
+        	   printf("%d - ",ss[j].notlar[i]);
+           }
+
+           printf("\n");
+    }
+    for (i = 0; i <5; i++){
+    	if (ss[i].numara == biggestStudentId(ss)){
+    		printf("Ortalamasý en yüksek olan ogrecinin adý:%s\n",ss[i].ad_soyad);
+    		printf("Ortalamasi en yüksek olan ogrencinin numarasý...:%d\n",biggestStudentId(ss));
+    		break;
+    	}
+
+    }
+   //printf("Ortalamasi en yüksek olan ogrencinin numarasý...:%d\n",biggestStudentId(ss));
+
+  return 0;
+}
+////soru 7:
+//output:
+//Fatih
+//sul
+//tan
+//mehmet
+//vakif
+//uni
+
 /*int main(void){
 
 	char str[80] = {"Fatih sul tan mehmet vakif uni"};
@@ -36,6 +111,11 @@
 	    return 0;
 }*/
 //soru 6:
+//output:
+//134 - 52 - 653 - 344 - 454 - 675 - 99 - 232 -
+//**************************************
+//23 - 232 - 99 - 675 - 454 - 344 - 653 - 52 -
+
 /*int main(void){
 
     int array1[] = {134,52,653,344,454,675,99,232};
@@ -60,10 +140,24 @@
 } */
 
 //soru 5:
+/*
+ * output:
+ * 45 (000000000023FE00)
+   67 (000000000023FE04)
+   89 (000000000023FE08)
+ * */
+/*int main(void){
+	int array[] = { 45, 67, 89 };
+	    int * array_ptr;
+	    array_ptr = array;
+	    printf("%i (%p)\n", *array_ptr, array_ptr);
+	    array_ptr++;
+	    printf("%i (%p)\n", *array_ptr, array_ptr);
+	    array_ptr = array_ptr + 1;
+	    printf("%i (%p)\n", *array_ptr, array_ptr);
+	    return 0;
 
-int main(void){
 
-
-}
+}*/
 
 
